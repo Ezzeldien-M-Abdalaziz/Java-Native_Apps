b839c698975554e27693df66e97395952140b753
Ezzeldien Magdy <139116899+Ezzeldien-M-Abdalaziz@users.noreply.github.com>
2025-11-24 00:58:34 +0200

    finish password generator

--- src/com/Ezzeldien/Passwords/PasswordGenerator.java
+++ src/com/Ezzeldien/Passwords/PasswordGenerator.java
@@ -5,29 +5,60 @@ import java.util.Random;
 
 public class PasswordGenerator {
 
+    private boolean hasOverLap(String s1 , String s2){
+        for(char c : s1.toCharArray()){
+            if(s2.contains(String.valueOf(c))){
+                return true;
+            }
+        }
+        return false;
+    }
+
     String generatedPassword(int length , boolean useLowercase, boolean useUppercase, boolean useSymbols  , boolean useNumbers) {
         String lowerCase = "abcdefghijklmnopqrstuvwxyz";
         String upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         String symbols = "~!@#$%^&*()_+-={}[]\\|;:'\",./<>?";
         String numbers = "0123456789";
 
-        HashMap<Integer, String> map = new HashMap<>();
-        map.put(0 , lowerCase);
-        map.put(1 , upperCase);
-        map.put(2 , symbols);
-        map.put(3 , numbers);
+        StringBuilder charsOptions = new StringBuilder();
+
+        if (useLowercase) charsOptions .append(lowerCase);
+        if (useUppercase) charsOptions.append(upperCase);
+        if (useSymbols) charsOptions.append(symbols);
+        if (useNumbers) charsOptions.append(numbers);
+
+        String allChars = charsOptions.toString();
+
+        while(true) {
+            boolean PasswordIsValid = true;
+            StringBuilder password = new StringBuilder();
+            Random rand = new Random();
+            for (int i = 0; i < length; i++) {
+                char nextLetter = allChars.charAt(rand.nextInt(allChars.length()));
+                password.append(nextLetter);
+            }
+
+            String generatedPassword = password.toString();
+
+            if( useLowercase && !hasOverLap(generatedPassword,lowerCase)){
+                PasswordIsValid = false;
+            };
+            if( useUppercase && !hasOverLap(generatedPassword,upperCase)){
+                PasswordIsValid = false;
+            };
+            if( useSymbols && !hasOverLap(generatedPassword,symbols)){
+                PasswordIsValid = false;
+            };
+            if( useNumbers && !hasOverLap(generatedPassword,numbers)){
+                PasswordIsValid = false;
+            };
 
 
-        StringBuilder password = new StringBuilder();
-        Random rand1 = new Random();
-        Random rand = new Random();
-
-        for (int i = 0; i < length; i++) {
-          String value =  map.get(rand1.nextInt(4));
-            char nextLetter = value.charAt(rand.nextInt(value.length()));
-            password.append(nextLetter);
+            if( PasswordIsValid){
+                return generatedPassword;
+            }else{
+                System.out.println( "Generated password doesn't meet the requirments " + generatedPassword + ", trying again");
+            }
         }
-
-        return password.toString();
     }
 }
