46bd0deb7afc62916df5b926e775559267af165a
Ezzeldien Magdy <139116899+Ezzeldien-M-Abdalaziz@users.noreply.github.com>
2025-11-28 18:28:52 +0200

    building weather forecast app

---
+++ src/com/Ezzeldien/WeatherForecastApp/WeatherApi.java
@@ -0,0 +1,7 @@
+package com.Ezzeldien.WeatherForecastApp;
+
+public class WeatherApi {
+
+    private static final String API_KEY = env.API_KEY;
+
+}
---
+++ src/com/Ezzeldien/WeatherForecastApp/env.java
@@ -0,0 +1,5 @@
+package com.Ezzeldien.WeatherForecastApp;
+
+public class env {
+    public static String API_KEY = "6dc58ed0cfb0deb00db15b9785aef2b1";
+}
